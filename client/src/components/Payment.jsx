2c6169730c17ad67bdb6b63557e72563b071cdb1
galibremo <galibremo@gmail.com>
2024-02-27 12:14:04 +0600

    Cash On Delivery functionality completed

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -134,7 +134,26 @@ export default function Payment() {
       toast.error(error);
     }
   }
-  function cashOnDeliveryHandler() {}
+  async function cashOnDeliveryHandler(e) {
+    e.preventDefault();
+
+    order.paymentInfo = {
+      type: "Cash On Delivery",
+    };
+
+    await axios
+      .post("/api/order/create-order", order, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        setOpen(false);
+        navigate("/order/success");
+        toast.success("Order successful!");
+        dispatch(emptyCartItem());
+      });
+  }
 
   return (
     <div>
@@ -368,4 +387,30 @@ const PaymentInfo = ({
     </div>
   );
 };
-const CartData = () => {};
+const CartData = ({ orderData }) => {
+  const shipping = orderData?.shipping?.toFixed(2);
+  return (
+    <div className="w-full bg-[#fff] rounded-md p-5 pb-8">
+      <div className="flex justify-between">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">subtotal:</h3>
+        <h5 className="text-[18px] font-[600]">${orderData?.subTotalPrice}</h5>
+      </div>
+      <br />
+      <div className="flex justify-between">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">shipping:</h3>
+        <h5 className="text-[18px] font-[600]">${shipping}</h5>
+      </div>
+      <br />
+      <div className="flex justify-between border-b pb-3">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">Discount:</h3>
+        <h5 className="text-[18px] font-[600]">
+          {orderData?.discountPrice ? "$" + orderData.discountPrice : "-"}
+        </h5>
+      </div>
+      <h5 className="text-[18px] font-[600] text-end pt-3">
+        ${orderData?.totalPrice}
+      </h5>
+      <br />
+    </div>
+  );
+};
